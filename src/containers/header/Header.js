e307ab11b68ba4caf5d05cbb56bf0a04a65613f9
Marlon Gomes <mrlon.mra@gmail.com>
2023-07-10 14:57:57 -0300

    Update Header.js

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import people from "../../assets/people.png";
 import ai from "../../assets/ai.png";
 import "./header.css";
@@ -8,89 +9,63 @@ function Header() {
     mensagem: "",
   });
 
-  const [response, setResponse] = useState({
-    type: "",
-    mensagem: "",
-  });
+  function handleInputChange(event) {
+    campos[event.target.name] = event.target.value;
+    setCampos(campos);
+  }
 
-  const onChangeInput = (e) =>
-    setCampos({ ...campos, [e.target.name]: e.target.value });
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    console.log("E-mail enviado! ", campos);
+    send();
+  }
 
-  const sendContact = async (e) => {
-    e.preventDefault();
-    try {
-      const res = await fetch("/send-email", {
-        method: "POST",
-        body: JSON.stringify(campos),
-        headers: { "Content-Type": "application/json" },
+  function send() {
+    const formData = new FormData();
+    Object.keys(campos).forEach((key) => formData.append(key, campos[key]));
+    axios
+      .post("/send-email", formData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        alert("E-mail enviado com sucesso!");
+        console.log("RESPOSTA JSON: ", response.data);
       });
-
-      const responseEnv = await res.json();
-
-      if (responseEnv.erro) {
-        setResponse({
-          type: "error",
-          mensagem: responseEnv.mensagem,
-        });
-      } else {
-        setResponse({
-          type: "success",
-          mensagem: responseEnv.mensagem,
-        });
-
-        setCampos({
-          mensagem: "",
-        });
-      }
-    } catch (err) {
-      setResponse({
-        type: "error",
-        mensagem: "Erro: Tente mais tarde!",
-      });
-    }
-  };
+  }
 
   return (
     <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
         <h1 className="gradient__text">
-          Estamos vivendo uma revolução tecnológica! Não fique para trás.
+          Estamos vivendo uma revolução tecnologica! Não fique para trás.
         </h1>
         <p>
-          Olá, meu nome é Marlon! Sou desenvolvedor há alguns anos e este será
-          meu projeto para apresentar meu portfólio!{" "}
-          <b>Caso queira entrar em contato, deixe seu e-mail abaixo!</b>
+          Olá, meu nome é Marlon! Sou desenvolvedor a alguns anos e este será
+          meu projeto para apresentar meu portfolio!{" "}
+          <b>Caso queira entrar em contato deixe seu e-mail abaixo!</b>
         </p>
 
-        <form onSubmit={sendContact}>
+        <form onSubmit={handleFormSubmit}>
           <div className="gpt3__header-content__input">
             <input
               type="email"
               id="mensagem"
               name="mensagem"
               placeholder="Digite seu e-mail."
-              onChange={onChangeInput}
-              value={campos.mensagem}
+              onChange={handleInputChange}
               required
             />
             <button type="submit">Entrar em Contato</button>
           </div>
         </form>
 
-        {response.type === "error" && (
-          <p className="alert-danger">{response.mensagem}</p>
-        )}
-
-        {response.type === "success" && (
-          <p className="alert-success">{response.mensagem}</p>
-        )}
-
         <div className="gpt3__header-content__people">
           <img src={people} alt="pessoas" />
-          <p>Mais de 150 clientes atendidos nessa trajetória!</p>
+          <p>Mais de 150 clientes atendidos nessa tragetória!</p>
         </div>
       </div>
-
       <div className="gpt3__header-image">
         <img src={ai} alt="ai" />
       </div>
