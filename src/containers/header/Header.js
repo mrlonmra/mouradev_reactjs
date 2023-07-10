10839e2db7ad723e57eb32abca5c94b605751d42
Marlon Gomes <mrlon.mra@gmail.com>
2023-07-10 15:10:07 -0300

    Update Header.js

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -20,7 +20,7 @@ function Header() {
     e.preventDefault();
     try {
       const res = await fetch("/send-email", {
-        method: "POST",
+        method: "GET",
         body: JSON.stringify(campos),
         headers: { "Content-Type": "application/json" },
       });
